669148e9b71f93716ef441577e56cf7c84cc71c2
Michael Natividad <Michael.Natividad@playngo.com>
2024-08-22 12:51:53 +0800

    listen to env port

--- server.js
+++ server.js
@@ -124,6 +124,8 @@ async function getSuperImages() {
     return images.data;
 }
 
-server.listen(3000, "0.0.0.0", () => {
-    console.log('server running at http://localhost:3000');
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
 });
